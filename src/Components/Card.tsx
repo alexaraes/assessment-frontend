e3563ef6d43abcc0ba8300dcbae8575a41955917
Alexa Rae Schreffler <alexarae@alexas-mbp.lan>
2022-11-12 20:25:34 -0500

    custom hook for page fetch

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import useFetch from "../hooks/useFetch";
 
 interface CardProps {
     id: string;
 }
 
 const Card = ({id}: CardProps) => {
+    const {response, error, isLoading} = useFetch(id);
+    console.log(response);
+
     return <div>{id}</div>
 }
 
--- src/app.tsx
+++ src/app.tsx
@@ -1,36 +1,16 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import Card from './Components/Card';
+import Card from './components/Card';
+
 
 // returns index.html if no base url provided
-// without baseurl, redirects to localhost:3000, not 3030
+// without specifying, forces base url to localhost:3000, not 3030
 const baseUrl = 'http://localhost:3030';
 
 const App = () => {
     const { id } = useParams<{ id: string }>();
-    const [data, setData] = useState(undefined);
-
-    useEffect(() => {
-        axios.get(`${baseUrl}/page/${id}`)
-        .then(function (response) {
-            // handle success
-            console.log(response.data);
-            setData(response.data);
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-        .finally(function () {
-            console.log('success!');
-        });
-    }, []);
 
     return (
-        <div>
-            <Card id={id} />  
-        </div>
+        <Card id={id} />
     );
 };
 
